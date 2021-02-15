3cc2e18d9122e93d8e4521744c70aedfcdf5346d
leon <leon@localhost.localdomain>
2021-02-15 17:53:24 +0200

    test

---
+++ pages/p.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="container">
+    <nuxt-link to="pla">Ahmed</nuxt-link>
+<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+<tbody></tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+    </table> </div>
+</template>
+<script>
+
+export default {
+        name: "index",
+
+data() {
+          return {
+              dataTable: null,
+              externalLoaded: false
+          }
+        },
+        mounted() {
+          console.log('table mounted');
+            if(this.dataTable === null){
+                this.onScriptLoaded();
+            }
+        },
+        methods: {
+            onScriptLoaded() {
+                this.externalLoaded = true;
+                console.log('script loaded');
+                    this.dataTable = $('#example').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "http://localhost:3001/res",
+            "dataType": "jsonp"
+        }
+    });
+            }
+           
+        }
+
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
